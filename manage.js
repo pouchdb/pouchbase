d44fff79924d6780052020c15199999dc862e968
Maciej Kisiel <mk319362@students.mimuw.edu.pl>
2014-03-24 23:00:43 +0100

    Add application key support

--- manage.js
+++ manage.js
@@ -4,21 +4,23 @@
 
 "use strict";
 
+var crypto = require('crypto');
 var fs = require('fs');
+var url = require('url');
 
 var async = require('async');
 var commander = require('commander');
-var request = require('request');
-var url = require('url');
 var yaml = require('js-yaml');
+var uuid = require('node-uuid');
+var request = require('request');
 
 var config = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
 
-function createAppDatabase(callback) {
-  process.stdout.write('Creating app database...');
+function createDatabase(dbName, callback) {
+  process.stdout.write('Creating ' + dbName + ' database...');
   request({
     method: 'PUT',
-    uri: url.format(db) + config.APP_DB,
+    uri: url.format(db) + dbName,
     auth: adminAuth
   }, function(err, res, body) {
     if (err || (res.statusCode !== 201 && res.statusCode !== 412)) {
@@ -34,15 +36,35 @@ function createAppDatabase(callback) {
   });
 }
 
+function secureDatabase(dbName, callback) {
+  process.stdout.write('Securing ' + dbName + ' database...');
+  var securityDoc = {
+    admins: {names: [], roles: []},
+    readers: {names: [], roles: ["_admin"]}
+  };
+  request({
+    method: 'PUT',
+    uri: url.format(db) + dbName + '/_security',
+    auth: adminAuth,
+    json: securityDoc
+  }, function (err, res, body) {
+    if (err || res.statusCode !== 200) {
+      callback(true);
+    } else {
+      console.log('OK');
+      callback(null);
+    }
+  });
+}
+
 commander
   .version('0.0.1')
   .option('--username <value>', 'CouchDB admin username')
   .option('--password <value>', 'CouchDB admin password')
   .option('--configure', 'configure a clean install of couch-persona')
+  .option('--add_dev', 'add a developer')
   .option('--register_app <name>', 'register a new app')
-  .option('--type <type>', 'app type')
-  .option('--desc <desc>', 'app description')
-  .option('--unregister_app <name>', 'unregister app')
+  .option('--app_dev <dev_id>', 'id of the owner of the app')
   .parse(process.argv);
 
 if (!commander.username || !commander.password) {
@@ -55,7 +77,10 @@ var adminAuth = {user: commander.username, pass: commander.password};
 
 if (commander.configure) {
   async.waterfall([
-    createAppDatabase
+    createDatabase.bind(this, config.APP_DB),
+    secureDatabase.bind(this, config.APP_DB),
+    createDatabase.bind(this, config.DEV_DB),
+    secureDatabase.bind(this, config.DEV_DB),
   ], function (err) {
     if (err) {
       console.log('ERROR');
@@ -64,76 +89,52 @@ if (commander.configure) {
   });
 }
 
-if (commander.register_app) {
-  if (commander.type === undefined) {
-    console.log('App type is required');
-    commander.help();
-  }
-  var name = commander.register_app;
-  var type = commander.type;
-  var description = commander.desc || '';
-  var appUrl = url.format(db) + config.APP_DB + '/' + name;
-
-  process.stdout.write('Registering app...');
+if (commander.add_dev) {
+  process.stdout.write('Adding developer...');
+  var devId = crypto.createHash('md5').update(uuid.v1()).digest('hex');
   request({
-    method: 'GET',
-    uri: appUrl,
+    method: 'PUT',
+    uri: db.format(db) + config.DEV_DB + '/' + devId,
     auth: adminAuth,
-  }, function (err, res, body) {
-    if (err || (res.statusCode !== 200 && res.statusCode !== 404)) {
-      console.log('ERROR');
-      process.exit(0);
-    } else if (res.statusCode === 200) {
-      console.log('already registered');
-      process.exit(0);
-    } else {
-      request({
-        method: 'PUT',
-        uri: appUrl,
-        auth: adminAuth,
-        json: {
-          _id: name,
-          type: type,
-          description: description
-        }
-      }, function(err, res, body) {
-        if (err) {
-          console.log('ERROR');
-        } else {
-          console.log('OK');
-        }
-        process.exit(0);
-      });
+    json: {
+      _id: devId,
     }
+  }, function(err, res, body) {
+    if (err || res.statusCode !== 201) {
+      console.log('ERROR');
+    } else {
+      console.log('OK\ndev id: ' + devId);
+    }
+    process.exit(0);
   });
 }
 
-if (commander.unregister_app) {
-  process.stdout.write('Unregistering app...');
-  var appUrl = url.format(db) + config.APP_DB + '/' + commander.unregister_app;
+if (commander.register_app) {
+  if (commander.app_dev === undefined) {
+    console.log('Developer ID is required');
+    commander.help();
+  }
+  var name = commander.register_app;
+  var appKey = crypto.createHash('md5').update(name).update(uuid.v1())
+      .digest('hex');
+
+  process.stdout.write('Registering app...');
 
   request({
-    method: 'GET',
-    uri: appUrl,
+    method: 'PUT',
+    uri: url.format(db) + config.APP_DB + '/' + appKey,
     auth: adminAuth,
-    json: true
-  }, function (err, res, body) {
-    if (err || res.statusCode !== 200) {
-      console.log('ERROR');
-      process.exit(0);
-    } else {
-      request({
-        method: 'DELETE',
-        uri: appUrl + '?rev=' + body._rev,
-        auth: adminAuth,
-      }, function(err, res, body) {
-        if (err) {
-          console.log('ERROR');
-        } else {
-          console.log('OK');
-        }
-        process.exit(0);
-      });
+    json: {
+      _id: appKey,
+      name: name,
+      dev: commander.app_dev
     }
+  }, function(err, res, body) {
+    if (err || res.statusCode !== 201) {
+      console.log('ERROR');
+    } else {
+      console.log('OK\napp key: ' + appKey);
+    }
+    process.exit(0);
   });
 }
