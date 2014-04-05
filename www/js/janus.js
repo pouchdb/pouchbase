ad24331e8a1b17edeb6ee4e847a8224107f9182f
Dale Harvey <dale@arandomurl.com>
2014-04-05 15:20:17 +0100

    (#19) - Wireframe of website flow

---
+++ www/js/janus.js
@@ -0,0 +1,14 @@
+
+var content = document.getElementById('content');
+
+function hashChanged() {
+  var hash = document.location.hash.slice(1) || 'home';
+  var contents = document.getElementById('page-' + hash);
+  if (!contents) {
+    return content.innerHTML = 'You gone and screwed up';
+  }
+  content.innerHTML = '<div id="' + hash + '">' + contents.innerHTML + '</div>';
+}
+
+window.addEventListener('hashchange', hashChanged);
+hashChanged();
