4efac04db294f6563c3241aac89dcbcb100642d4
Dale Harvey <dale@arandomurl.com>
2014-04-22 03:48:00 +0100

    (#29) - Add an initial client library and start integrating to website

--- www/js/janus-webapp.js
+++ www/js/janus-webapp.js
@@ -1,14 +1,24 @@
 
-var content = document.getElementById('content');
+(function() {
 
-function hashChanged() {
-  var hash = document.location.hash.slice(1) || 'home';
-  var contents = document.getElementById('page-' + hash);
-  if (!contents) {
-    return content.innerHTML = 'You gone and screwed up';
+  var content = document.getElementById('content');
+
+  function hashChanged() {
+    var hash = document.location.hash.slice(1) || 'home';
+    var contents = document.getElementById('page-' + hash);
+    if (!contents) {
+      return content.innerHTML = 'You gone and screwed up';
+    }
+    content.innerHTML = '<div id="' + hash + '">' + contents.innerHTML + '</div>';
   }
-  content.innerHTML = '<div id="' + hash + '">' + contents.innerHTML + '</div>';
-}
 
-window.addEventListener('hashchange', hashChanged);
-hashChanged();
+  window.addEventListener('hashchange', hashChanged);
+  hashChanged();
+
+})();
+
+var janus = new Janus('developer');
+
+document.getElementById('persona').addEventListener('click', function() {
+  janus.login();
+});
