d7b0773eb0aaa7e205ce3fb99397e4d4e54f2493
Dale Harvey <dale@arandomurl.com>
2014-10-29 15:06:47 +0100

    Clean slate

--- bin/dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./node_modules/.bin/watchify www/js/janus.js -s Janus -o www/js/janus-client.js &
-./node_modules/.bin/nodemon ./lib/janus.js
--- bin/run-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ASSERT_URL='http://localhost:5555/verify'
-
-./node_modules/mocha/bin/mocha tests/test*.js
